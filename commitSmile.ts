246386d4c7ee704e33fc277cea37429f2417b371
INEEDJ <ineedjobtostartwork@gmail.com>
2025-03-13 23:55:15 +0100

    ⚙️  Chore(enviroment): change `commitSmile.ts` config

--- commitSmile.ts
+++ commitSmile.ts
@@ -3,6 +3,10 @@ import { defaultConfig } from "commitsmile";
 export default defaultConfig({}).deepMerge({
   prompts: {
     description: false,
-    scopes: { custom: true, workspaces: true, options: ["🌍 Enviroment"] },
+    scopes: {
+      custom: true,
+      workspaces: true,
+      options: [{ label: "🌍 Enviroment", value: "enviroment" }],
+    },
   },
 });
